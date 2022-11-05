ff490d19a231dc74bf7fbc4ca31e973daae74376
ayorich <kayodele17@yahoo.com>
2022-11-05 20:27:06 +0100

    update

--- controllers/sketchController.js
+++ controllers/sketchController.js
@@ -2,7 +2,7 @@ const Sketch = require('./../models/sketchModel');
 const factory = require('./handlerFactory');
 
 exports.createSketch = factory.createOne(Sketch);
-exports.getSketch = factory.getOne(Sketch);
+exports.getSketch = factory.getOne(Sketch, 'user');
 exports.getAllSketches = factory.getAll(Sketch);
 exports.updateSketch = factory.updateOne(Sketch);
 exports.deleteSketch = factory.deleteOne(Sketch);
--- models/sketchModel.js
+++ models/sketchModel.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 
 const sketchSchema = new mongoose.Schema({
-  sketch: [{ x: Number, y: Number }],
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'Booking must belong to a user!']
-  }
+  name: {
+    type: String,
+    required: [false, 'Please provide sketch name']
+  },
+  sketch: mongoose.Schema.Types.Mixed,
+  user: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Booking must belong to a user!']
+    }
+  ]
 });
 
 const Sketch = mongoose.model('Sketch', sketchSchema);
